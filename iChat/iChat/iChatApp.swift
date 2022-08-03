999b279553af4ee9ee0807c0f9337fe43bfedee4
Darío Fernando Gallegos Quishpe <dfgallegos@eservicios.indra.es>
2022-08-03 11:46:49 +0200

    Basic project struct

---
+++ iChat/iChat/iChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatApp.swift
+//  iChat
+//
+//  Created by Darío Gallegos on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct iChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
