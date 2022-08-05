3626f4a85bacfcda0c9b288ecdd39f2c912ae41c
Darío Fernando Gallegos Quishpe <dfgallegos@eservicios.indra.es>
2022-08-05 13:18:10 +0200

    Add TextField to send messages

--- iChat/iChat/View/Home.swift
+++ iChat/iChat/View/Home.swift
@@ -11,11 +11,43 @@ struct Home: View {
     @ObservedObject var homeModel = HomeModel()
     var body: some View {
         VStack {
-            ScrollView {
-                ForEach(homeModel.messages) { message in
-                    ChatRow(chatData: message)
+            
+            //NavigationBar
+            
+            ScrollViewReader { reader in
+                ScrollView {
+                    VStack(spacing: 16) {
+                        ForEach(homeModel.messages) { message in
+                            ChatRow(chatData: message)
+                        }
+                    }
                 }
             }
+            
+            //NavBar
+            HStack(spacing: 16) {
+                TextField("Enter Message", text: $homeModel.inputText)
+                    .padding(.horizontal)
+                    .frame(height: 45)
+                    .background(Color.primary.opacity(0.06))
+                    .clipShape(Capsule())
+                
+                if homeModel.inputText != "" {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "paperplane.fill")
+                            .font(.system(size: 22))
+                            .foregroundColor(.white)
+                            .frame(width: 45, height: 45)
+                            .background(Color.blue)
+                            .clipShape(Circle())
+                    }
+
+                }
+            }
+            .animation(.default, value: 0.1)
+            .padding()
         }
         .onAppear(perform: {
             homeModel.onAppear()
